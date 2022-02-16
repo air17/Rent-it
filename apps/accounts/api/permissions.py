e0c1ff8170e7fcc85fed651307ce9bc999f75f19
air17 <ou1705@gmail.com>
2022-02-16 20:27:51 +0300

    Fix an issue
    Using IsAdmin instead of custom IsSuperuser.

--- apps/accounts/api/permissions.py
+++ apps/accounts/api/permissions.py
@@ -5,8 +5,3 @@ class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user:
             return obj == request.user
-
-
-class IsSuperUser(BasePermission):
-    def has_permission(self, request, view):
-        return request.user and request.user.is_superuser
--- apps/accounts/api/views.py
+++ apps/accounts/api/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
-from .permissions import IsOwner, IsSuperUser
+from .permissions import IsOwner
 from .serializers import UserSerializer
 
 
@@ -10,13 +10,13 @@ class UserViewSet(ModelViewSet):
 
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = [
-        IsAuthenticated,
-    ]
+    permission_classes = (IsAuthenticated,)
 
     def get_permissions(self):
         if self.action in ("list", "destroy"):
-            self.permission_classes += (IsSuperUser,)
+            self.permission_classes += (IsAdminUser,)
         elif self.action == "update":
             self.permission_classes += (IsOwner,)
+        elif self.action == "create":
+            self.permission_classes = (AllowAny,)
         return super().get_permissions()
